d3bfccdc2ba11f6c50d875f862b7258a2989bed7
Poriya 's <poriyaaditya@Poriyas-MacBook-Air.local>
2026-08-16 17:32:18 +0530

    Serve uploaded media files in production

--- mehndi_project/urls.py
+++ mehndi_project/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path("", include("website.urls")),
 ]
 
+# Serve uploaded media files
 urlpatterns += static(
     settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
+    document_root=settings.MEDIA_ROOT,
 )
